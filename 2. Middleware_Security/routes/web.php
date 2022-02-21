b19372ef285c5a3d0e3acbad1dd84e6f42722f6e
mdiktushar <md.tushar1804@gmail.com>
2022-02-21 15:16:22 +0600

    middelware commit 1

--- 2. Middleware_Security/routes/web.php
+++ 2. Middleware_Security/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/admin/user/roles', ['middleware'=>['role'], function () {
+    return "middleware role";
+}]);
